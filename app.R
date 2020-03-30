7d51d0f9e3d9f2b92184d9c16a59884270e3555a
xindyhu <xindy.hu@gmail.com>
2020-03-29 21:31:28 -0700

    try no.2, superhero working version

--- app.R
+++ app.R
@@ -1,34 +1,42 @@
-library('shiny')
+library(shiny)
+library(shinythemes)
+library(shinycssloaders)
 source("src/helper_county.R")
 # Global variables can go here
-fips <- '06001'
+fips <- ''
+zip <- "94587"
 nppl <- 20
 
-css <- HTML("
-.html-widget.gauge svg {
-  height: 400px;
-  width: 800px;
-}")
+css <- HTML(".html-widget.gauge svg {height: 350px;width: 900px;}")
 
 # Define the UI
-ui <- fluidPage(
+ui <- fluidPage(theme=shinytheme("superhero"),
   titlePanel("COVID-19 Risk Score Calculator"),
   tags$head(tags$style(css)),
   #INPUT
   sidebarLayout(
     sidebarPanel(
-      helpText("Answer a few questions to see your COVID-19 risk score:"),
+      helpText("Answer a few questions to see your COVID-19 risk score:", class = "lead"),
       textInput('fips', label =  '5-digit FIPS code of your county', fips),
+      textInput('zip', label =  "If you don't know your county FIPS code, what's your 5-digit zip code?", zip),
       numericInput('nppl', 'Number of contacts you have had in the past two weeks', nppl),
-      sliderInput('fac_underreport', "Choose what percentage of cases are tested?", min = 0.01, max = 1, value = 0.15, step = 0.01),
+      #sliderInput('fac_underreport', "Choose what percentage of cases are tested?", min = 0.01, max = 1, value = 0.15, step = 0.01),
       checkboxInput('is_sick', "Are you sick already?"),
-      checkboxInput('in_hosp', "Do you work in a hospital?"),
-      checkboxInput("in_highriskzone", "Do you live in or have you visited in the past two weeks an area where the transmission is widespread?")
+      #checkboxInput('in_hosp', "Do you work in a hospital?"),
+      checkboxInput('in_highriskzone', "Do you live in or have you visited in the past two weeks an area where the transmission is widespread?"),
+      actionButton('go', "Calculate my risk score", class = "btn-primary"),
+      width =3
     ),
     #OUTPUT
     mainPanel(
-      gaugeOutput("gauge"),
-      textOutput('res')
+      tabsetPanel(
+        tabPanel("Plot",
+                 fluidRow(withSpinner(gaugeOutput("gauge"), type = 1), style = "height:350px;"),
+                 fluidRow(textOutput("res"), style = "width:800px")),
+        #tabPanel("Map"),
+        tabPanel("Methodology",
+                 textOutput("methods"))),
+      width = 9
     )
   )
 )
@@ -36,19 +44,29 @@ ui <- fluidPage(
 
 # Define the server code
 server <- function(input, output) {
-  temp<- reactive({
-    fips<-input$fips
+  temp<- eventReactive(input$go, {
+    #read in FIPS or get it from ZIP
+    if(input$fips!=""){
+      fips<-input$fips
+    }else{
+      fips<-get_fips_from_zip(input$zip)
+    }
+    #get county-level characteristics
     county_pop <- get_county_pop(fips)
     county_name <- get_county_name(fips)
-    county_casecount <- get_county_casecount(fips, Sys.Date()-1)
+    county_casecount <- get_county_casecount(fips, latest_day)
     county_underreport <- calc_county_underreport(fips)
-    risk <- 1-(1-get_county_casecount(fips, Sys.Date()-1)/get_county_pop(fips)/calc_county_underreport(fips))^input$nppl
+    if (input$nppl>0){
+      risk <- 1-(1-get_county_casecount(fips, latest_day)/get_county_pop(fips)/calc_county_underreport(fips))^input$nppl
+    } else{
+      risk <- 0
+    }
     g<-function(x){
       # a mapping function to address nonlinearity between probability and score
-      assertthat::assert_that(x>0 && x<1)
+      assertthat::assert_that(x>=0 && x<=1)
       return((log(x)+8)/8)
     }
-    score<-if_else(risk>0, g(risk), 0)
+    score<-if_else(risk>0, g(risk)*100, 0)
     
     unlist(list("fips" = fips,
                 "county_pop" = county_pop,
@@ -61,19 +79,25 @@ server <- function(input, output) {
   })
   output$gauge <-renderGauge({
     temp <- temp()
-    gauge(temp['score']%>%as.numeric()%>%round(2), 
-          min = 0, max = 1, 
-          sectors = gaugeSectors(success = c(0, 0.3),
-                                 warning = c(0.3, 0.7),
-                                 danger = c(0.7, 1)))
+    score <- round(as.numeric(temp['score']))
+    gauge(score, 
+          min = 0, max = 100, 
+          sectors = gaugeSectors(success = c(0, 30),
+                                 warning = c(30, 70),
+                                 danger = c(70, 100)),
+          label = "")
   })
   output$res <-renderText({
     temp <- temp()
-    paste('You live in county:', temp['county_name'], ".",
-          'Your county has', temp['county_casecount'], 'cases out of a population of', temp['county_pop'], '.',
-          'We estimated the under-reporting factor is', temp['county_underreport'], '.',
-          "The probability of you contracting COVID-19 is", temp['risk'], '.',
-          "On a scale of 0 to 1, your risk score is", temp['score'], '.')
+    paste('You live in county:', temp['county_name'], '.',
+          'Your county has', temp['county_casecount'], 'cases out of a population of', 
+          format(temp['county_pop']%>%as.numeric(), big.mark = ','), '.',
+          'We estimated the under-reporting factor is', scales::percent(temp['county_underreport']%>%as.numeric()), '.',
+          "The probability of you contracting COVID-19 is", scales::percent(temp['risk']%>%as.numeric()), '.',
+          "On a scale of 0 to 100, your risk score is", round(temp['score']%>%as.numeric()), '.')
+  })
+  output$methods <-renderText({
+    "Methodology placeholder (George)."
   })
 }
 
