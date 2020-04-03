7146361f0e901eb3f389ce88105d1a1bb833ecd5
xindyhu <xindy.hu@gmail.com>
2020-04-02 23:19:27 -0700

    feat: implement updated cormobidity OR data

--- app.R
+++ app.R
@@ -15,24 +15,30 @@ prob_flu<- 35.5/327.2/26 #assume 26 weeks of flu season
 # infection hospitalization rate and fatality rate, https://www.thelancet.com/journals/laninf/article/PIIS1473-3099(20)30243-7/fulltext
 age_list   = c(0,       10,      20,      30,      40,      50,      60,     70,     80)
 hosp_list  = c(0,       0.0408,  1.04,    3.43,    4.25,    8.16,    11.8,   16.6,   18.4) / 100
-#case to infection conversion 1.38% cfr, 0.657% infection fatality rate
+#case to infection conversion 1.38% cfr, 0.657% infection fatality rate, use CDC ICU list
 icu_list   = c(0,       0,       2,       2,       3.7,     5.05,    6.4,    9.3,    8.4) *.657/1.38 /100
 death_list = c(0.00161, 0.00695, 0.0309,  0.0844,  0.161,   0.595,   1.93,   4.28,   7.8)  / 100
 
 # odds ratios, https://www.cdc.gov/mmwr/volumes/69/wr/mm6913e2.htm
-diabetes_or = c(5.00, 4.57)
-lung_or     = c(2.53, 2.83)
-cvd_or      = c(6.60, 4.88)
-immune_or   = c(2.58, 2.86)
-renal_or    = c(10.17, 5.82)
-pregnant_or = c(1.23, 0.42)
-neuro_or    = c(6.18, 2.30)
-liver_or    = c(2.44, 3.05)
-other_or    = c(4.21, 3.33)
-smoker_or   = c(2.67, 2.64)
-all_conditions_death_or = 27.84
+# CCDC weekly, 2020 Vol No.2
+#The Epidemiological Characteristics of an Outbreak of 2019 Novel
+#Coronavirus Diseases (COVID-19) — China, 2020
+#The Novel Coronavirus Pneumonia Emergency Response Epidemiology Team
+# first is hospitalization odds ratio, second is ICU odds ratio, third is death odds ratio
+diabetes_or = c(5.00, 4.57, 4.70)
+hyper_or    = c(5.00, 4.57, 4.70) # use the same as diabetes
+lung_or     = c(2.53, 2.83, 3.59)
+cvd_or      = c(6.60, 4.88, 7.43)
+immune_or   = c(2.58, 2.86, 6.11) # death OR not available in CCDC
+renal_or    = c(10.17, 5.82, 6.11) # death OR not available in CCDC
+#pregnant_or = c(1.23, 0.42, 1)
+#neuro_or    = c(6.18, 2.30, 6.11) # death OR not available in CCDC
+#liver_or    = c(2.44, 3.05, 6.11) # death OR not available in CCDC
+other_or    = c(4.21, 3.33, 6.11) # death OR not available in CCDC
+smoker_or   = c(2.67, 2.64, 6.11) # death OR not available in CCDC
+#all_conditions_death_or = 27.84
 # OR source: https://www.medrxiv.org/content/10.1101/2020.02.24.20027268v1
-male_or     = c(1.8518, 1.85)
+male_or     = c(1.8518, 1.85, 1.69)
 
 # CSS
 css <- HTML(".html-widget.gauge svg {height: 66%; width: 66%; display: block; margin-left: auto;margin-right: auto; margin-bottom:-10%;}
@@ -83,9 +89,6 @@ ui <- fluidPage(theme=shinytheme("superhero"),
                              "Cardiovascular disease" = "is_cvd",
                              "Immunocompromised condition" = "is_immune",
                              "Chronic renal disease" = "is_renal",
-                             "Pregnancy" = "is_pregnant",
-                             "Neurologic disorder" = "is_neuro",
-                             "Chronic liver disease" = "is_liver",
                              "Other chronic disease" = "is_other",
                              "Current or former smoker" = "is_smoker"
                              )),
@@ -173,16 +176,16 @@ server <- function(input, output, session) {
       condition_root = substr(condition_id, 4, nchar(condition_id))
       hosp_odds = hosp_odds * eval(parse(text=paste0(condition_root, "_or[1]")))
       icu_odds = icu_odds * eval(parse(text=paste0(condition_root, "_or[2]")))
+      death_odds = death_odds * eval(parse(text=paste0(condition_root, "_or[3]")))
     }
     if (input$gender == "male") {
       hosp_odds = hosp_odds * male_or[1]
       icu_odds = icu_odds * male_or[2]
+      death_odds = death_odds * male_or[3]
     }
     
-    if (input$has_preexisting) {
-      death_odds = death_odds * all_conditions_death_or
-    } else {
-      # clear the conditional panel's UI
+    if (!input$has_preexisting) {
+      # clear the conditional panel's UI when unchecked
       updateCheckboxGroupInput(session, "conditions", selected = character(0))
     }
 
