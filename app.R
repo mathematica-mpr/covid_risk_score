417e46779fc96835980a31f6df253aac728880e7
George Luo <georgecluo@gmail.com>
2020-04-06 12:16:17 -0700

    moved most non-UI code out of app.R

--- app.R
+++ app.R
@@ -5,7 +5,8 @@ library(shinyBS)
 library(shinyjs)
 source("src/helper_county.R")
 source("src/global_var.R")
-source("src/methods.R")
+source("src/info_html.R")
+source("src/results.R")
 
 # Define the UI
 ui <- fluidPage(
@@ -77,6 +78,7 @@ ui <- fluidPage(
       tags$div(class="sharethis-inline-share-buttons"),
       width = 3
     ), # sidebarPanel
+    
     # OUTPUT
     mainPanel(
       tabsetPanel(
@@ -110,122 +112,38 @@ server <- function(input, output, session) {
     validate(need(!is.na(fips), "Check input!"))
     #fix NYC, all NYC borough data uses NY county
     if (fips%in%NY_fips_ls){
-      county_pop <- NY_fips_ls%>%map(~get_county_pop(.))%>%unlist()%>%sum()
-      county_name <- "New York City (5 Boroughs)"
-      county_casecount <- get_county_casecount("36061", latest_day)
-      county_underreport <- calc_county_underreport("36061")
+      population <- NY_fips_ls%>%map(~get_county_pop(.))%>%unlist()%>%sum()
+      name <- "New York City (5 Boroughs)"
+      casecount <- get_county_casecount("36061", latest_day)
+      underreport_factor <- calc_county_underreport("36061")
     } else{
       #get county-level characteristics
-      county_pop <- get_county_pop(fips)
-      county_name <- get_county_name(fips)
-      county_casecount <- get_county_casecount(fips, latest_day)
-      county_underreport <- calc_county_underreport(fips)
+      population <- get_county_pop(fips)
+      name <- get_county_name(fips)
+      casecount <- get_county_casecount(fips, latest_day)
+      underreport_factor <- calc_county_underreport(fips)
     }
     
     return (list(fips = fips,
-                 county_pop = county_pop,
-                 county_name = county_name,
-                 county_casecount = county_casecount,
-                 county_underreport = county_underreport))
+                 population = population,
+                 name = name,
+                 casecount = casecount,
+                 underreport_factor = underreport_factor))
   })
   
-  calculateRisk <- reactive({
+  updateRisk <- reactive({
     county_data<-getCountyData()
-    county_casecount<-county_data$county_casecount
-    county_pop<-county_data$county_pop
-    county_underreport<-county_data$county_underreport
-    total_covid_count = county_casecount * county_underreport
-    
-    #risk calculator
-    if(input$is_sick){
-      # if you're already sick with flu-like symptoms, your likelihood of having covid is P(C19) / (P(C19) + P(flu))
-      total_covid_probability = total_covid_count / county_pop
-      exposure_risk = total_covid_probability / (total_covid_probability + prob_flu)
-    } else if (input$nppl>0) {
-      # ASSUMPTION: diagnosed cases are not active
-      active_casecount = total_covid_count - county_casecount 
-    
-      # ASSUMPTION: active community case count cannot be less than 10% of reported cases
-    if (active_casecount < 0.1 * county_casecount) {
-        active_casecount = 0.1 * county_casecount
-    }
-      prev_active<-active_casecount/county_pop #prevalence of active cases
-      exposure_risk <- 1-(1-prev_active*transmissibility_household)^(input$nppl+input$nppl2*transmissibility_household)
-    } else{
-      exposure_risk <- 0
-    }
-    
-    
-    # susceptibility calculations
-    risk2odds<-function(prob) {
-      return (prob / (1 - prob))
-    }
-    odds2risk<-function(odds) {
-      return (odds / (1 + odds))
-    }
-    
-    # exposure modifier
-    if(input$hand){
-      exposure_risk<-odds2risk(risk2odds(exposure_risk)*hand_or)
-    }
-    
-    if(input$ppe){
-      exposure_risk<-odds2risk(risk2odds(exposure_risk)*ppe_or)
-    }
-    
-    age = as.numeric(input$age)
-    validate(need(age >= 0, "Invalid age."))
-    age_index = max(which(age_list <= age))
-    hosp_prob = hosp_list[age_index]
-    icu_prob = icu_list[age_index]
-    death_prob = death_list[age_index]
-    hosp_odds = risk2odds(hosp_prob)
-    icu_odds = risk2odds(icu_prob)
-    death_odds = risk2odds(death_prob)
-    
-    for (condition_id in input$conditions) {
-      # remove "is_" prefix
-      condition_root = substr(condition_id, 4, nchar(condition_id))
-      hosp_odds = hosp_odds * eval(parse(text=paste0(condition_root, "_or[1]")))
-      icu_odds = icu_odds * eval(parse(text=paste0(condition_root, "_or[2]")))
-      death_odds = death_odds * eval(parse(text=paste0(condition_root, "_or[3]")))
-    }
-    if (input$gender == "male") {
-      hosp_odds = hosp_odds * male_or[1]
-      icu_odds = icu_odds * male_or[2]
-      death_odds = death_odds * male_or[3]
-    }
-    
     if (!input$has_preexisting) {
       # clear the conditional panel's UI when unchecked
       updateCheckboxGroupInput(session, "conditions", selected = character(0))
     }
-
-    hosp_risk = odds2risk(hosp_odds)
-    icu_risk = odds2risk(icu_odds)
-    death_risk = odds2risk(death_odds)
-
-    g<-function(exposure, hospitalization, icu, death){
-      x = exposure * (hospitalization + icu + death) 
-      x_flu = prob_flu * (hosp_flu + icu_flu + death_flu)
-      # a mapping function to better visualize probability
-      normalized<-log10(x/x_flu)*50/3+50
-      # 50 means equal disease burden as flu
-      # 100 means 1000 times worse than flu
-      # 0 means 1/1000 times the disease burden of flu
-      return(normalized)
-    }
-    score<-if_else(exposure_risk>0, g(exposure_risk, hosp_risk, icu_risk, death_risk), 1)
-    return (list(county_data = county_data,
-                 exposure_risk = exposure_risk,
-                 hosp_risk = hosp_risk,
-                 icu_risk = icu_risk,
-                 death_risk = death_risk,
-                 score = score))
+    
+    # in results.R
+    return (calculateRisk(input, county_data))
   })
   
   output$gauge <-renderGauge({
-    risk<-calculateRisk()
+    risk<-updateRisk()
     gauge(case_when(risk$score<1 ~ 1,
                     risk$score>100 ~ 100,
                 TRUE ~round(risk$score)), 
@@ -237,148 +155,26 @@ server <- function(input, output, session) {
   })
   
   output$res <-renderUI({
-    risk <- calculateRisk()
-    county_data = risk$county_data
-    county_casecount<-county_data$county_casecount
-    county_pop<-county_data$county_pop
-    county_underreport<-county_data$county_underreport
-    exposure_risk<-risk$exposure_risk
-    
-    formatDynamicString <- function(string) {
-      return (tags$b(tags$span(style="color:#F0AD4E",string)))
-    }
-    formatNumber<-function(number, unit) {
-      return (formatDynamicString(HTML(paste0(formatC(signif(number,digits=2), digits=2,format="fg"), unit))))
-    }
-    formatPercent<-function(probability) {
-      return (formatNumber(100 * probability, "%"))
-    }
-    
-    prob_flu_string = formatPercent(prob_flu)
-    county_underreport_string = formatNumber(county_underreport, "x")
-    risk_string = formatPercent(exposure_risk)
-
-    sickness_html = tags$p(HTML(paste0(
-      "Your estimated probability of catching COVID-19 through community transmission is ", risk_string, '. ',
-      "For comparison, ", prob_flu_string, ' of Americans catch the flu every week during flu season.')))
-    
-    score<- risk$score
-    score = max(score, 1)
-    score = min(score, 100)
-    score_string = tags$p(HTML(paste0(
-      "Your risk score is ",
-      formatDynamicString(round(score)), 
-      case_when(
-        score<30 ~ paste0(
-          ", which is (relatively) safe. Even so, it's a good time to make sure that you're ",
-          tags$a("prepared! ", href = urls$cdc_get_ready)),
-        score>70 ~ paste0(
-          ", which is quite serious. Avoiding exposure, practicing good hygiene, and making sure you have ",
-          tags$a("a plan in place ", href = urls$cdc_prevention), 
-          "are critically important for you."),
-        TRUE ~ paste0(
-          ". Please take the time to review ",
-          tags$a("this page", href = urls$cdc_prevention),
-          " to make sure you're well prepared in the days to come.")
-      )
-    )))
-    
-    if (input$is_sick == TRUE) {
-      sickness_html = tags$p(HTML(paste0(
-        "Since you're already sick, please immediately consult ", 
-        tags$a("the CDC's instructions", href = urls$cdc_if_sick),
-        ", or walk through their ",
-        tags$a("self-checker", href = urls$cdc_chatbot),
-        ". The probability that you could have COVID-19 is ", risk_string, '. ')))
-    }
-    
-    tagList(
-      tags$p(""),
-      tags$p(HTML(paste0(
-        'We found data from ', formatDynamicString(county_data$county_name), ' for your zip code.',
-        ' This county has ', formatDynamicString(format(county_casecount, big.mark=",")), ' cases out of a population of ', 
-        formatDynamicString(format(county_pop, big.mark = ',')), " as of ", formatDynamicString(latest_day), 
-        ", and we estimated that your county's specific under-reporting factor is ", 
-        county_underreport_string, '. '))),
-      sickness_html,
-
-      tags$p(HTML(paste0(
-        "If you were to get sick from COVID-19, your risk of hospitalization is ", 
-        formatPercent(risk$hosp_risk),
-        ", your risk of requiring an ICU is ",
-        formatPercent(risk$icu_risk),
-        ", and your risk of dying is ",
-        formatPercent(risk$death_risk), "."
-      ))),
-      score_string
-      # fluidRow(column(width = 4,
-      #                 offset = 2,
-      #                 tags$a(href=urls$facebook_button,
-      #                        "Share on Facebook",
-      #                        class="btn btn-facebook btn-block"),
-      #                 includeScript(urls$facebook_widget)),
-      #          # column(width = 3,
-      #          #        offset = 1,
-      #          #        tags$a(href=urls$w_button, "Share on WhatsApp", class="btn btn-success btn-block")
-      #          #        ),
-      #          column(width = 4,
-      #                 offset = 2,
-      #                 tags$a(href=urls$twitter_button, "Share on Twitter", class="btn btn-info btn-block"),
-      #                 includeScript(urls$twitter_widget)))
-      
-    )
+    risk <- updateRisk()
+    # in src/results.R
+    renderResultsHtml(risk, input$is_sick)
   })
   
   output$methods <-renderUI({
-    getMethodsHtml()
+    # in src/info_html.R
+    renderMethodsHtml()
   })
   
-  faqQuestion<- function(string) {
-    return (tags$li(tags$b(tags$span(style="color:#DF691A", string))))
-  }
   output$faq <- renderUI({
-    tagList(
-      tags$h3("Frequently Asked Questions:"),
-      faqQuestion("Why is my score so high?"),
-      tags$p("We wanted our tool to be sensitive to the wide variety of circumstances encountered in the US right now;",
-             "as a result, it's calibrated around a score of 50. A score of 50 is defined as an equal disease burden as ",
-             "the flu, estimated based on total number of flu cases, hospitalizations, ICU admission, and deaths in the ",
-             "2018-2019 flu season. For every 10x change in (Exposure*Susceptibility), the score will change by 50/3.",
-             "Thus, for two users, one with a score of 20, and one with a score of 70, the user with a score of 70 is",
-             "1000x more likely to have been exposed to COVID-19 and experience a serious consequence (hospitalization, ",
-             "ICU admission, or death)."),
-      faqQuestion("My family is sheltering in place with me. Should I count them as exposure risks?"),
-      tags$p("As long as your family has been sheltering in place with you, you should be able to think of your family",
-             "as a single \"user\" of the tool. However, bear in mind that their exposure risks become yours, and vice",
-             "versa."),
-      faqQuestion("My county only has a few (tens, hundreds, thousands) of cases. Why is my exposure risk so high?"),
-      tags$p("Probably the most difficult/controversial/inaccurate part of our calculator is our estimation of the",
-             "underreporting factor, the factor we use to estimate the true, larger, community prevalence of COVID-19",
-             "in your community. In some places, our tool may be overestimating this factor, and in some places, it",
-             "may be underestimating. Even so, it's probably good enough to get you a ballpark estimate of your risk."),
-      tags$p("If your community has seen a huge increase in testing, has a \"test positive\" rate < 5%, and if you",
-             "feel like anyone that wants to be tested is being tested promptly, then I think there is reason to",
-             "believe that the authorities are tracking most of the community cases of COVID-19 in your area.",
-             "Unfortunately, that is not true of most of the US at present."),
-      faqQuestion("My specific medical condition isn't listed. What do I do?"),
-      tags$p("Try using \"other conditions\" to get a catch-all estimate of your susceptibility."),
-      faqQuestion("My hospitalization/ICU/death risk seems out of whack."),
-      tags$p("A lot is still unknown about the disease, and data sets are sparse, so our susceptibility scores are",
-             "good for ballpark estimates only. We'll update our tool with better numbers as they become available."),
-      faqQuestion("I have suggestion X, or know of data set Y, or want feature Z..."),
-      tags$p("Let us know at", tags$a("covid.risk.score@gmail.com", href="mailto:covid.risk.score@gmail.com"), "!")
-    )
+    # in src/info_html.R
+    renderFaqHtml()
   })
   
   output$about <- renderUI({
-    tagList(
-      tags$h3("About us:"),
-      tags$p(tags$a("Cindy Hu ", href = "https://www.linkedin.com/in/xindi-cindy-hu-harvard/"), "is a data scientist trained in public health."),
-      tags$p(tags$a("George Luo", href = "https://www.linkedin.com/in/george-luo-38852450/"), "is a software engineer.")
-    )
+    # in src/info_html.R
+    renderAboutHtml()
   })
 }
 
-  
 # Return a Shiny app object
 shinyApp(ui = ui, server = server)
---
+++ src/info_html.R
@@ -0,0 +1,95 @@
+library(shiny)
+source("src/global_var.R")
+
+# render the Methodology
+renderMethodsHtml <- function() {
+  # return
+  tagList(
+    tags$p(""),
+    tags$p('Our "Risk Score" visualization is the quantity {Exposure * Susceptibility}, normalized by the average disease burden of flu, logarithmically scaled.'),
+    tags$p("Exposure represents how likely it is that you've been infected with the virus. It's a function of the prevalence of active cases in your
+             community and ",
+           tags$a("transmissibility estimates.", href = urls$cdc_mm6909e1),
+           "You can reduce your exposure by ",
+           tags$a("social distancing, practicing good hygiene, and closely following the directives of your local public health officials.",
+                  href = urls$cdc_prevention),
+           "Your personal susceptibility to COVID-19 is quantified by {P(hospitalization) + P(ICU) + P(death)}.",
+           "Please remember that even if your personal susceptibility is low, you can still help by preventing spread to others."
+    ),
+    tags$p(""),
+    tags$h4("Assumptions:"),
+    tags$ol(
+      tags$li(
+        "To calculate exposure, we used ",
+        tags$a("the New York Times's published data on COVID-19 cases & deaths", href = urls$nytimes_data_article),
+        "to estimate the prevalence of infected people within your county. 
+        For the five boroughs in New York City, we use the overall New York City COVID-19 data.",
+      ),
+      tags$li(
+        "Due to rapid spread and insufficient testing during the COVID-19 pandemic, there are likely additional unreported cases beyond the officially reported cases.",
+        "We followed methodology reported by", tags$a("Russell et al (2020)", href = urls$russel_etal_2020),
+        "to calculate the percentage of cases that are currently known, and presumably quarantined, versus the number of cases still distributed throughout the community."),
+      tags$li("Other methods of becoming infected (e.g. touching an infected surface) are not accounted for by this calculator."),
+      tags$li(
+        "Estimations of the probability of hospitalization, ICU and death among all infected cases, stratified by age groups, were obtained from a Lancet article authored by ",
+        tags$a("Verity et al (2020).", href = urls$verity_etal_2020),
+        "We chose this study over US CDC reports because this study is larger and more thorough. We do not account for differences between Chinese population and US population."
+      ),
+      tags$li("Estimations of risk factors associated with underlying medical conditions were obtained from",
+              tags$a("China CDC weekly, 2020 Vol No.2", href = urls$ccdc_vol2_2020), "and gender from this preprint by", 
+              tags$a("Caramelo et al (2020).", href = urls$caramelo_etal_2020)
+      ),
+    ),
+    tags$p(""),
+    tags$p("We'll be doing our best to update these assumptions as additional knowledge about the virus becomes available.")
+  )
+}
+
+# helper function for rendering FAQ's
+faqQuestion<- function(string) {
+  return (tags$li(tags$b(tags$span(style="color:#DF691A", string))))
+}
+
+# render the FAQ's
+renderFaqHtml <- function() {
+  # return
+  tagList(
+    tags$h3("Frequently Asked Questions:"),
+    faqQuestion("Why is my score so high?"),
+    tags$p("We wanted our tool to be sensitive to the wide variety of circumstances encountered in the US right now;",
+           "as a result, it's calibrated around a score of 50. A score of 50 is defined as an equal disease burden as ",
+           "the flu, estimated based on total number of flu cases, hospitalizations, ICU admission, and deaths in the ",
+           "2018-2019 flu season. For every 10x change in (Exposure*Susceptibility), the score will change by 50/3.",
+           "Thus, for two users, one with a score of 20, and one with a score of 70, the user with a score of 70 is",
+           "1000x more likely to have been exposed to COVID-19 and experience a serious consequence (hospitalization, ",
+           "ICU admission, or death)."),
+    faqQuestion("My family is sheltering in place with me. Should I count them as exposure risks?"),
+    tags$p("As long as your family has been sheltering in place with you, you should be able to think of your family",
+           "as a single \"user\" of the tool. However, bear in mind that their exposure risks become yours, and vice",
+           "versa."),
+    faqQuestion("My county only has a few (tens, hundreds, thousands) of cases. Why is my exposure risk so high?"),
+    tags$p("Probably the most difficult/controversial/inaccurate part of our calculator is our estimation of the",
+           "underreporting factor, the factor we use to estimate the true, larger, community prevalence of COVID-19",
+           "in your community. In some places, our tool may be overestimating this factor, and in some places, it",
+           "may be underestimating. Even so, it's probably good enough to get you a ballpark estimate of your risk."),
+    tags$p("If your community has seen a huge increase in testing, has a \"test positive\" rate < 5%, and if you",
+           "feel like anyone that wants to be tested is being tested promptly, then I think there is reason to",
+           "believe that the authorities are tracking most of the community cases of COVID-19 in your area.",
+           "Unfortunately, that is not true of most of the US at present."),
+    faqQuestion("My specific medical condition isn't listed. What do I do?"),
+    tags$p("Try using \"other conditions\" to get a catch-all estimate of your susceptibility."),
+    faqQuestion("My hospitalization/ICU/death risk seems out of whack."),
+    tags$p("A lot is still unknown about the disease, and data sets are sparse, so our susceptibility scores are",
+           "good for ballpark estimates only. We'll update our tool with better numbers as they become available."),
+    faqQuestion("I have suggestion X, or know of data set Y, or want feature Z..."),
+    tags$p("Let us know at", tags$a("covid.risk.score@gmail.com", href="mailto:covid.risk.score@gmail.com"), "!")
+  )
+}
+
+renderAboutHtml <- function() {
+  tagList(
+    tags$h3("About us:"),
+    tags$p(tags$a("Cindy Hu ", href = "https://www.linkedin.com/in/xindi-cindy-hu-harvard/"), "is a data scientist trained in public health."),
+    tags$p(tags$a("George Luo", href = "https://www.linkedin.com/in/george-luo-38852450/"), "is a software engineer.")
+  )
+}
