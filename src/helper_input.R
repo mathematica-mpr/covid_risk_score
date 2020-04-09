39fff701bf62616edb91d993b4203eafe36275a3
Cindy Hu <xindy.hu@gmail.com>
2020-04-08 22:18:08 -0700

    small refactor to output

--- src/helper_input.R
+++ src/helper_input.R
@@ -54,7 +54,7 @@ collapseStory <- function() {
       title = "3. Your Behavior",
       sliderInput('nppl', 
                   'How many people (include your household members) do you come into close contact (> 10 min, < 6 feet) with in a week?', 
-                  min = 0, max = 100, value = 0, step =1),
+                  min = 0, max = 100, value = 1, step =1),
       checkboxInput('is_roommate', "I live with other people."),
       conditionalPanel(
         condition = "input.is_roommate == true",
