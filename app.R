a5eb24044abe697b0380a79a192323922c6f3393
Emma Pendl-Robinson <EPendl-Robinson@mathematica-mpr.com>
2020-06-18 17:18:34 -0400

    feat. change moving count range

--- app.R
+++ app.R
@@ -116,16 +116,16 @@ server <- function(input, output, session) {
       population <- NY_fips_ls%>%map(~get_county_pop(.))%>%unlist()%>%sum()
       name <- "New York City (5 Boroughs)"
       casecount <- get_county_casecount("36061", latest_day)
-      casecount_day1_14 <- get_county_moving_casecount("36061", 0, 14)
-      casecount_day14_28 <- get_county_moving_casecount("36061", 14, 28)
+      casecount_newer <- get_county_moving_casecount("36061", 0, 21)
+      casecount_older <- get_county_moving_casecount("36061", 21, 42)
       underreport_factor <- calc_county_underreport("36061")
     } else{
       #get county-level characteristics
       population <- get_county_pop(fips)
       name <- get_county_name(fips)
       casecount <- get_county_casecount(fips, latest_day)
-      casecount_day1_14 <- get_county_moving_casecount(fips, 0, 14)
-      casecount_day14_28 <- get_county_moving_casecount(fips, 14, 28)
+      casecount_newer <- get_county_moving_casecount(fips, 0, 21)
+      casecount_older <- get_county_moving_casecount(fips, 21, 42)
       underreport_factor <- calc_county_underreport(fips)
     }
     
@@ -133,8 +133,8 @@ server <- function(input, output, session) {
                  population = population,
                  name = name,
                  casecount = casecount,
-                 casecount_day1_14 = casecount_day1_14,
-                 casecount_day14_28 = casecount_day14_28,
+                 casecount_newer = casecount_newer,
+                 casecount_older = casecount_older,
                  underreport_factor = underreport_factor))
   })
 
