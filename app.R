7ea355497c9985117accf050c78d111dbf8ae6d0
Erin Lipman <ELipman@mathematica-mpr.com>
2020-08-13 13:57:30 -0400

    feat: Remove special logic for getting NYC population data b/c USAFacts reports buroughs separately

--- app.R
+++ app.R
@@ -112,23 +112,13 @@ server <- function(input, output, session) {
       fips <-input$fips0
     }
     
-    #fix NYC, all NYC borough data uses NY county
-    if (fips%in%NY_fips_ls){
-      population <- NY_fips_ls%>%map(~get_county_pop(.))%>%unlist()%>%sum()
-      name <- "New York City (5 Boroughs)"
-      casecount <- get_county_casecount("36061", latest_day)
-      casecount_newer <- get_county_moving_casecount("36061", 0, 14)
-      casecount_older <- get_county_moving_casecount("36061", 14, 28)
-      underreport_factor <- calc_county_underreport("36061")
-    } else{
-      #get county-level characteristics
-      population <- get_county_pop(fips)
-      name <- get_county_name(fips)
-      casecount <- get_county_casecount(fips, latest_day)
-      casecount_newer <- get_county_moving_casecount(fips, 0, 14)
-      casecount_older <- get_county_moving_casecount(fips, 14, 28)
-      underreport_factor <- calc_county_underreport(fips)
-    }
+    #get county-level characteristics
+    population <- get_county_pop(fips)
+    name <- get_county_name(fips)
+    casecount <- get_county_casecount(fips, latest_day)
+    casecount_newer <- get_county_moving_casecount(fips, 0, 14)
+    casecount_older <- get_county_moving_casecount(fips, 14, 28)
+    underreport_factor <- calc_county_underreport(fips)
     
     return (list(fips = fips,
                  population = population,
