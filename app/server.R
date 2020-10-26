75307b925bfe6da1a069238748a87c8494c9fc35
Margaret Luo <MLuo@mathematica-mpr.com>
2020-10-26 13:14:47 -0400

    feat: remove validate of api_return message so entire reactive get_risk_info is evaluated so gauge and res outputs are updated

--- app/server.R
+++ app/server.R
@@ -46,70 +46,80 @@ server <- function(input, output, session) {
     validate(need(input$go > 0, ""))
     validate(need(nchar(input$zip) >= 5, ""))
     api_return <- calculateRisk(input)
-    
-    # if a message is returned, display the message
-    validate(need(is.null(api_return$message), gsub("Bad Request: ","\nPlease Correct:\n",as.character(api_return$message))))
-    
-    api_out <- api_return$results
-    
-    # number of outputs
-    n_out <- length(api_out)
 
-    # if the length of output is more than one have user select one county
-    if(n_out > 1 ){
-      output$select_county <- renderUI({
-      validate(need(is.null(input$ordinal_county), ""))
-      tagList(
-        tags$h4("There is more than one county that matches your 5-digit zip code."),
-        tags$h4("Please choose a county:")
-      )})
-      output$zipcontrol <- renderUI({
-        validate(need(is.null(input$ordinal_county), ""))
-        list_opts <- as.character(1:length(api_out))
-        county_names<-sapply(api_out, `[`, "name") %>% as.character()
-        radioButtons("ordinal_county", label = "Counties:", 
-                     choiceNames = county_names, choiceValues  = list_opts, selected = character(0))
+    if (is.null(api_return$message)) {
+      api_out <- api_return$results
+      
+      # number of outputs
+      n_out <- length(api_out)
+      
+      # if the length of output is more than one have user select one county
+      if(n_out > 1 ){
+        output$select_county <- renderUI({
+          validate(need(is.null(input$ordinal_county), ""))
+          tagList(
+            tags$h4("There is more than one county that matches your 5-digit zip code."),
+            tags$h4("Please choose a county:")
+          )})
+        output$zipcontrol <- renderUI({
+          validate(need(is.null(input$ordinal_county), ""))
+          list_opts <- as.character(1:length(api_out))
+          county_names<-sapply(api_out, `[`, "name") %>% as.character()
+          radioButtons("ordinal_county", label = "Counties:", 
+                       choiceNames = county_names, choiceValues  = list_opts, selected = character(0))
         })
+        
+        # stop if county is not selected
+        validate(need(!is.null(input$ordinal_county), ""))
+        
+        which_county<- as.numeric(input$ordinal_county)
+        
+      } else {
+        # if there is only one output county, select the first output county
+        which_county <- 1}
       
-      # stop if county is not selected
-      validate(need(!is.null(input$ordinal_county), ""))
+      one_county <- api_out[[which_county]]
       
-      which_county<- as.numeric(input$ordinal_county)
-      
-    } else {
-      # if there is only one output county, select the first output county
-      which_county <- 1}
-    
-    one_county <- api_out[[which_county]]
-    
-    return (one_county)
+      return (one_county)
+    } else  {
+      return(api_return$message)
+    }   
   })
-
+  
   output$gauge <-renderGauge({
     
     risk<-get_risk_info()
     
-    validate(need(!is.null(risk), ""))
-    
-    output$output_intro <- renderUI({
-      # in app/results.R
-      renderOutputIntroHtml()
-    })
-    
-    gauge(case_when(risk$score<1 ~ 1,
-                    risk$score>100 ~ 100,
-                TRUE ~round(risk$score)), 
-          min = 0, max = 100, 
-          sectors = gaugeSectors(success = c(0, 30),
-                                 warning = c(30, 70),
-                                 danger = c(70, 100)),
-          label = "")
+    if (grepl("Bad Request", paste0(risk, collapse = ", "))) {
+      output$output_intro <- renderUI({
+          HTML(paste0(risk, collapse = "<br/>"))
+      })
+      
+    } else {
+      validate(need(!is.null(risk), ""))
+      
+      output$output_intro <- renderUI({
+        # in app/results.R
+        renderOutputIntroHtml()
+      })
+      
+      gauge(case_when(risk$score<1 ~ 1,
+                      risk$score>100 ~ 100,
+                      TRUE ~round(risk$score)), 
+            min = 0, max = 100, 
+            sectors = gaugeSectors(success = c(0, 30),
+                                   warning = c(30, 70),
+                                   danger = c(70, 100)),
+            label = "")
+    }
   })
   
   output$res <-renderUI({
     risk <- get_risk_info()
     # in app/results.R
-    renderResultsHtml(risk, input$symptoms, input$hand, input$ppe)
+    if (!grepl("Bad Request", paste0(risk, collapse = ", "))) {
+      renderResultsHtml(risk, input$symptoms, input$hand, input$ppe)
+    } 
   })
   
   output$methods <-renderUI({
